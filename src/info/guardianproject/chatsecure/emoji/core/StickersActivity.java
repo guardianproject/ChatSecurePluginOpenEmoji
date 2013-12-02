e1115d3e7368ebb8642a9e2854fbc7ac1772b15c
n8fr8 <nathan@freitas.net>
2013-12-02 17:04:42 -0500

    adding first project files

---
+++ src/info/guardianproject/chatsecure/emoji/core/StickersActivity.java
@@ -0,0 +1,10 @@
+package info.guardianproject.chatsecure.emoji.core;
+import android.app.Activity;
+
+
+/*
+ * this is an empty placeholder activity for sticker plugins
+ */
+public class StickersActivity extends Activity {
+
+}
